550e39adc615850bfc5bf8f043a3da0f15efde8c
Neil Schemenauer <nas@arctrix.com>
2025-10-16 10:23:13 -0700

    Fix test that uses Py_REFCNT as an l-value.

--- test/upcast.cpp
+++ test/upcast.cpp
@@ -13,7 +13,17 @@ int main()
 {
     PyTypeObject o;
     Y y;
+    // Test that upcast preserves object identity by comparing pointers.
+#if PY_VERSION_HEX >= 0x030a0000
+    // In Python >= 3.10, Py_REFCNT cannot be used as an l-value.  Instead, we
+    // directly compare the object pointers. For types directly derived from
+    // PyObject (like PyTypeObject), the addresses should match. For Y, which
+    // has multiple inheritance, upcast correctly adjusts to the PyObject base.
+    BOOST_TEST(boost::python::upcast<PyObject>(&o) == reinterpret_cast<PyObject*>(&o));
+    BOOST_TEST(boost::python::upcast<PyObject>(&y) == static_cast<PyObject*>(&y));
+#else
     BOOST_TEST(&Py_REFCNT(boost::python::upcast<PyObject>(&o)) == &Py_REFCNT(&o));
     BOOST_TEST(&Py_REFCNT(boost::python::upcast<PyObject>(&y)) == &Py_REFCNT(&y));
+#endif
     return boost::report_errors();
 }
